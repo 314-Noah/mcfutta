33028ca2ddde202d4ce0c73b37e55487466fa06e
314-Noah <60508844+314-Noah@users.noreply.github.com>
2020-03-16 12:09:03 +0100

    Update orders.js

--- src/main/resources/static/js/orders.js
+++ src/main/resources/static/js/orders.js
@@ -1,6 +1,15 @@
 
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
 function update(id) {
 	send(id, 'orders/increment');
+    sleep(1000);
 	location.reload(true);
 }
 
